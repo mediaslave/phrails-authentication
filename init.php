c6235f32ccb8e21365fa7cc479d7386bf69b9f47
Dave Kerschner <docgnome@docgno.me>
2011-01-20 17:53:46 -0800

    New autoload

--- init.php
+++ init.php
@@ -7,11 +7,22 @@ add_include_directory($dirname . '/lib');
 
 spl_autoload_register('mediaslave_autoload', true, true);
 
-function mediaslave_autoload($class_name){
+function mediaslave_autoload($class_name) {
+	$ns = 'net\\mediaslave\\authentication\\';
+
 	$class_parts = explode('\\', $class_name);
+
 	$class_name = array_pop($class_parts);
-	$loaded = @include_once($class_name . '.php');
-	if(!$loaded){
-		//print $class_name . '<br/>';
+
+	$cpi = implode('\\', $class_parts);
+	$cpi =	str_replace($ns, '', $cpi);
+	$class = strtolower($cpi);
+	$file =  '/' . str_replace('\\', '/', $class) . '/' . $class_name . '.php' ;
+
+	if(file_exists(__DIR__ . $file)) {
+		$loaded = require_once(__DIR__ . $file);
+		if(!$loaded){
+			//print $class_name . '<br/>';
+		}
 	}
 }
