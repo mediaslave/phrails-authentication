4e583ead318f033bdf978d0b54c58310793e8564
mediaslave <justin@mediaslave.net>
2011-11-23 11:28:44 -0800

    Added findAllSettings.

--- app/models/User.php
+++ app/models/User.php
@@ -61,7 +61,6 @@ class User extends \Model{
 		$s->rule('password', new \PregRule('%^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,13}$%', 'Password should include one lower case letter, one upper case letter, one digit, 6-15 characters in length, and no spaces.'));
 
 	}
-
 	/**
 	 * Does the user have the appropriate roles?
 	 *
--- app/models/UserSettingThruUser.php
+++ app/models/UserSettingThruUser.php
@@ -7,9 +7,9 @@ namespace net\mediaslave\authentication\app\models;
  * PageBlock
  */
 /**
- * ClassBlock
+ * ClASsBlock
  */
-class UserSettingThruUser extends \Model{
+clASs UserSettingThruUser extends \Model{
 
 
 	/**
@@ -26,4 +26,28 @@ class UserSettingThruUser extends \Model{
     $s->belongsTo('user')
       ->className('net\mediaslave\authentication\app\models\User', true);
 	}
+
+  /**
+   *
+   * Find the user settings
+   *
+   * @return array
+   * @author Justin Palmer
+   **/
+  public function findAllSettings($id)
+  {
+    return $this->findBySql('SELECT user_setting.*
+                            FROM `user_setting_thru_users` AS `user_setting`
+                            INNER JOIN  `users` AS `user`
+                                ON `user`.id = `user_setting`.user_id
+                            INNER JOIN `user_roles` AS `role`
+                                ON `user`.id = `role`.user_id
+                            INNER JOIN `user_setting_thru_roles` AS `rthru`
+                                ON `role`.role_id = `rthru`.role_id
+                            INNER JOIN `user_settings` AS `setting`
+                                ON `rthru`.user_setting_id = `setting`.id
+                            WHERE `user`.id = ?
+                            GROUP BY `user_setting`.user_id, `user_setting`.user_setting_id',
+                            array($id));
+  }
 }
