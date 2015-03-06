5e366a1f0d31184e65cc1f7f265bb497efb6d712
Bryan Everson <b_everson@yahoo.com>
2015-03-06 15:27:00 -0800

    Alter error message shown from PregRule for password to reflect longer possible password length

--- app/models/User.php
+++ app/models/User.php
@@ -71,7 +71,7 @@ class User extends \Model{
 		$s->rule('email', new \EmailRule());
 		$s->rule('email', new \UniqueDatabaseRule());
 
-		$s->rule('password', new \PregRule('%^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,37}$%', 'Password should include one lower case letter, one upper case letter, one digit, 6-15 characters in length, and no spaces.'));
+		$s->rule('password', new \PregRule('%^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,37}$%', 'Password should include one lower case letter, one upper case letter, one digit, 6-40 characters in length, and no spaces.'));
 	}
 	/**
 	 * Does the user have the appropriate roles?
